f29d8690d60c1509104b9a7812f9308a357af470
takasakirina <119413759+takasakirina@users.noreply.github.com>
2022-11-29 20:03:48 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+var tomoriru_setsuna = [
+  "index.thml",
+  "/icon-192x192.png",
+  "/icon-256x256.png",
+  "/icon-384x384.png",
+  "/icon-512x512.png",
+  "/manifest.json",
+  "/xlsx.full.min.js",
+  "/QR_SSEd.png"
+]
+
+
+
+self.addEventListener('install',function(e) {
+  e.waitUntil(
+    caches.open(tomoriru_setsuna).then(function(cache) {
+      return cache.addAll(tomoriru_setsuna)
+    })
+
+  )
+})
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondwith(
+    caches.match(event.request).then(function(response) {
+      
+      //if(cache != tomoriru_setsuna){
+      if(navigator.onLine != false || cache != tomoriru_setsuna){
+        return caches.open(tomoriru_setsuna).then(function(cache){
+          return cache.match(event.request)
+        })
+      }
+
+      return response || fetch(event.request)
+    })
+  )
+})
